9358dad7c11d3c1ba8ef691a54ba2fb39f6f4ec6
adrian.garcia <adrian.garcia@lksnext.com>
2022-03-10 18:53:00 +0100

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Clase principal
+ * @author adrian.garcia
+ *
+ */
+
+public class Main {
+	
+	/**
+	 * Main method
+	 * @param args arguments of app
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("¡Ejecución completada con éxito!");
+	}
+
+}
